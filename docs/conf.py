f7defcd6693d1c1dedd7d68e943f02848a50e7a1
Manuel Kaufmann <humitos@gmail.com>
2020-04-15 16:18:48 +0200

    Allow building locally and use its API

--- docs/conf.py
+++ docs/conf.py
@@ -12,5 +12,12 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 hoverxref_auto_ref = True
 hoverxref_domains = ['py']
 
+if os.environ.get('READTHEDOCS') == 'True':
+    # Building on Read the Docs
+    hoverxref_api_host = 'https://readthedocs.org'
+if os.environ.get('LOCAL_READTHEDOCS') == 'True':
+    # Building on a local Read the Docs instance
+    hoverxref_api_host = 'http://community.dev.readthedocs.io'
+
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
