bfba0c0185256dd604aed2cbd4a47e3116edc5e7
Anthony Johnson <aj@ohess.org>
2020-07-15 02:03:20 -0600

    Again, smaller.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,9 @@
 
 # Default settings
 project = 'Test Builds'
+import os
+version = os.environ.get('READTHEDOCS_VERSION', '')
+release = version
 extensions = [
     'sphinx_autorun',
 ]
@@ -11,14 +14,3 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
-
-def setup(app):
-
-    def config_init(app, config):
-        current_version = config.html_context.get('current_version')
-        if current_version is not None:
-            config.version = current_version
-            config.release = current_version
-        return config
-
-    app.connect('config-inited', config_init)
