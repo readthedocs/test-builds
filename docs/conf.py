e86af44abd4f5002b40fe1035bbe41abcfefef89
Anthony Johnson <aj@ohess.org>
2020-07-15 00:54:36 -0600

    Test config with auto version/release

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,9 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+def setup(app):
+    current_version = app.config.html_context.get('current_version')
+    if current_version is not None:
+        app.config.version = current_version
+        app.config.release = current_version
