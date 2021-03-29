01abffda036b9a85e7b9bff08582dc5950141b2b
Santos Gallegos <stsewd@protonmail.com>
2021-03-29 14:29:29 -0500

    Print

--- docs/conf.py
+++ docs/conf.py
@@ -16,3 +16,4 @@ html_theme = 'sphinx_rtd_theme'
 ON_RTD = os.environ.get('READTHEDOCS')
 if ON_RTD:
     subprocess.run(['apt', 'install', '-y', 'cmatrix'], capture_output=True)
+    print("cmatrix installed")
