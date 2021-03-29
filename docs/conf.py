0950e1c2c74ebd170ef79a8ccaf305e563195f97
Santos Gallegos <stsewd@protonmail.com>
2021-03-29 14:21:52 -0500

    Use apt

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+import os
+import subprocess
 
 # Default settings
 project = 'Test Builds'
@@ -11,3 +12,7 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+ON_RTD = os.environ.get('READTHEDOCS')
+if ON_RTD:
+    subprocess.run(['apt', 'install', '-y', 'cmatrix'], capture_output=True)
