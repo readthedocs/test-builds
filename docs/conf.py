68bddca29a320cb797cde6990efd624216f52ec2
Manuel Kaufmann <humitos@gmail.com>
2021-06-28 10:06:16 +0200

    Update git-lfs to latest version

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ html_static_path = ['img']
 # Install and execute git-lfs
 import os
 if not os.path.exists('./git-lfs'):
-    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
-    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.13.3/git-lfs-linux-amd64-v2.13.3.tar.gz')
+    os.system('tar xvfz git-lfs-linux-amd64-v2.13.3.tar.gz')
     os.system('./git-lfs install')  # make lfs available in current repository
     os.system('./git-lfs fetch')  # download content from remote
     os.system('./git-lfs checkout')  # make local files to have the real content on them
