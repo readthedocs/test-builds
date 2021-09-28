26eb7d1f962f8df19895e564cd6b92bfa71cfce9
Manuel Kaufmann <humitos@gmail.com>
2021-09-28 20:24:18 +0200

    ln -s readthedocs config file

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,6 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+import os
+os.system('ln -s ../.readthedocs.yaml .')
