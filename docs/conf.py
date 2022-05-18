471f4a54f7a17ba349306abe297b0117f45804de
Manuel Kaufmann <humitos@gmail.com>
2022-05-18 16:11:26 +0200

    Cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# Default settings
 project = 'Test Builds'
 extensions = [
     'sphinx_autorun',
     'sphinx_js',
 ]
 
-latex_engine = 'xelatex'  # allow us to build Unicode chars
 js_source_path = '../src/'
 
 
