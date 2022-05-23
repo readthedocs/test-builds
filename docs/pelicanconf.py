9aec82186e3b3067a473c2cad1fb4f45a119b29a
Manuel Kaufmann <humitos@gmail.com>
2022-05-23 11:00:03 +0200

    Pelican for Read the Docs

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -1,12 +1,21 @@
+import os
+
 AUTHOR = '@humitos'
 SITENAME = 'test-builds'
-SITEURL = ''
 
-PATH = 'content'
+READTHEDOCS_PROJECT = os.environ.get('READTHEDOCS_PROJECT')
+READTHEDOCS_VERSION = os.environ.get('READTHEDOCS_VERSION')
+READTHEDOCS_LANGUAGE = os.environ.get('READTHEDOCS_LANGUAGE')
+
+# Define SITEURL properly to be hosted on Read the Docs on a path different than /
+# allowing mutliple versions.
+SITEURL = f'https://{READTHEDOCS_PROJECT}.readthedocs.io/{READTHEDOCS_LANGUAGE}/{READTHEDOCS_VERSION}/'
+
+PATH = 'docs'
 
 TIMEZONE = 'Europe/Rome'
-
 DEFAULT_LANG = 'en'
+OUTPUT_PATH = 'output/'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,4 +37,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
