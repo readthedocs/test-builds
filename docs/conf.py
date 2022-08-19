bac63cc6414ad05692d414d19957841af44c5462
Anthony Johnson <aj@ohess.org>
2022-08-19 01:49:24 -0700

    Replace with standalone library

--- docs/conf.py
+++ docs/conf.py
@@ -12,4 +12,4 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_js_files = ['vendor.js', 'app.js']
+html_js_files = ['readthedocs-doc-diff.js']
