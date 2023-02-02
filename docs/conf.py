f8a1efce29b4dc53ca63f90a1a8ed0ea7a0cd466
Manuel Kaufmann <humitos@gmail.com>
2023-02-02 13:46:25 +0100

    Build PDF with rinohtype

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
-
 # Default settings
 project = 'Test Builds'
 extensions = [
     'sphinx_autorun',
 ]
 
-latex_engine = 'xelatex'  # allow us to build Unicode chars
-
-
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+rinoh_documents = [
+    dict(
+        doc='index',             # top-level file (index.rst)
+        target='test-builds',    # output file (test-builds.pdf)
+    ),
+]
