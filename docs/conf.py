0da308d1fa33a9413b6fdbaaab8ac8ab03446f84
Benjamin Bach <benjamin@overtag.dk>
2023-04-28 20:31:57 +0200

    Enable sphinxcontrib-jquery

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@
 project = 'Test Builds'
 extensions = [
     'sphinx_autorun',
+    'sphinxcontrib-jquery',  # https://github.com/readthedocs/sphinx_rtd_theme/issues/1452
 ]
 
 latex_engine = 'xelatex'  # allow us to build Unicode chars
