11387120d6e66f3a9e3647727735558418e7707e
Benjamin Bach <benjamin@readthedocs.org>
2023-06-22 18:00:18 +0200

    Remove the deliberate failure from conf.py :)

--- docs/conf.py
+++ docs/conf.py
@@ -13,22 +13,10 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 html_theme = 'sphinx_rtd_theme'
 
 
-# Fail the PDF build on purpose by producing a syntax error
-#
-# ! Undefined control sequence.
-# l.71 \useage
-#             [titles]{}
-
-# ! LaTeX Error: Missing \begin{document}.
-latex_elements = {
-    'preamble': r'''
-\useage[titles]{}
-''',
-}
-
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-latex_documents
 # (startdocname, targetname, title, author, theme, toctree_only)
 latex_documents = [
    ('index', 'index.tex', 'test', "author", 'manual'),
-   ('contents', 'contents.tex', "test", "author", 'howto'),
+   ('index', 'index2.tex', 'test2', "author", 'manual'),
+#   ('contents', 'contents.tex', "test", "author", 'howto'),
 ]
