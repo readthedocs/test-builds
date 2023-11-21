ec58165cd8024e99f109b37c817838ff9bccd6b9
Manuel Kaufmann <humitos@gmail.com>
2023-11-21 17:29:46 +0100

    Trigger Read the Docs search when focus on input

---
+++ docs/javascripts/readthedocs.js
@@ -0,0 +1,5 @@
+// Trigger Read the Docs' search addon instead of Material MkDocs default
+document.querySelector(".md-search__input").addEventListener("focus", (e) => {
+  const event = new CustomEvent("readthedocs-search-show");
+  document.dispatchEvent(event);
+});
