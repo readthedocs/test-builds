472a1ea2e41fcc220231bd163cd440124d6eddaa
Manuel Kaufmann <humitos@gmail.com>
2023-11-21 17:40:08 +0100

    More updates

--- docs/javascripts/readthedocs.js
+++ docs/javascripts/readthedocs.js
@@ -1,5 +1,7 @@
-// Trigger Read the Docs' search addon instead of Material MkDocs default
-document.querySelector(".md-search__input").addEventListener("focus", (e) => {
-  const event = new CustomEvent("readthedocs-search-show");
-  document.dispatchEvent(event);
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Trigger Read the Docs' search addon instead of Material MkDocs default
+  document.querySelector(".md-search__input").addEventListener("focus", (e) => {
+    const event = new CustomEvent("readthedocs-search-show");
+    document.dispatchEvent(event);
+  });
 });
