8a26b70d404f0758c4343f3d904210145834f01e
Eric Holscher <eric@ericholscher.com>
2024-11-20 14:35:00 -0800

    Fix output

--- docs/next.config.js
+++ docs/next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
 }
 
 module.exports =
