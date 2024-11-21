f75e9128d0d811f14189d3dc916c31b167fdd801
Eric Holscher <eric@ericholscher.com>
2024-11-21 11:07:10 -0800

    Annoying

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,20 +1,20 @@
-const withMarkdoc = require('@markdoc/next.js');
+const withMarkdoc = require("@markdoc/next.js");
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
- 
+  output: "export",
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
 
-  // Ensure links are relative
-  basePath: process.env.READTHEDOCS_CANONICAL_URL ? new URL(process.env.READTHEDOCS_CANONICAL_URL).pathname : '',
-}
+  // Use Canonical URL, but only the path and with no trailing /
+  basePath: process.env.READTHEDOCS_CANONICAL_URL
+    ? new URL(process.env.READTHEDOCS_CANONICAL_URL).pathname.replace(/\/$/, "")
+    : "",
+};
 
-module.exports =
-  withMarkdoc({mode: 'static'})({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-    ...nextConfig, // Include nextConfig in the export
-  });
-
+module.exports = withMarkdoc({ mode: "static" })({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
+  ...nextConfig, // Include nextConfig in the export
+});
