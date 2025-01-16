888eaf176fa44dd753d8d0844887068d1cadb6be
Manuel Kaufmann <humitos@gmail.com>
2025-01-16 13:53:02 +0100

    Use a different name

--- docs/conf.py
+++ docs/conf.py
@@ -12,4 +12,4 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
 
-templates_path = [".templates"]
+templates_path = ["_templates"]
