0a7d59006c8178e802cf6b41b802b0b35a83afd6
Manuel Kaufmann <humitos@gmail.com>
2025-01-21 11:45:04 +0100

    Populate environment variables

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+import sphinx
+
 # Default settings
 project = 'Test Builds'
 extensions = [
@@ -12,3 +15,20 @@ html_theme = 'insipid'
 html_extra_path = ['static']
 
 templates_path = ["_templates"]
+
+
+# TODO: this should be done by the theme itself
+def extend_html_context(app, pagename, templatename, context, doctree):
+     # Add ``sphinx_version_info`` tuple for use in Jinja templates
+     context['sphinx_version_info'] = sphinx.version_info
+
+     # Inject all the Read the Docs environment variables in the context:
+     # https://docs.readthedocs.io/en/stable/reference/environment-variables.html
+     context['READTHEDOCS'] = os.environ.get("READTHEDOCS", False) == "True"
+     if context['READTHEDOCS']:
+         for key, value in os.environ.items():
+             if key.startswith("READTHEDOCS_"):
+                 context[key] = value
+
+def setup(app):
+    app.connect("html-page-context", extend_html_context)
