f59584be7df45da7d2fac8497fa4c82d65ec21aa
Manuel Kaufmann <humitos@gmail.com>
2025-01-21 11:40:36 +0100

    Simplified build with custom flyout positioning

--- docs/conf.py
+++ docs/conf.py
@@ -2,30 +2,13 @@
 project = 'Test Builds'
 extensions = [
     'sphinx_autorun',
-    'notfound.extension',
-    'hoverxref.extension',
-    'sphinx_js',
-    'sphinx_tabs.tabs',
-    'sphinx_jinja2',
 ]
 
 latex_engine = 'xelatex'  # allow us to build Unicode chars
 
-autosectionlabel_prefix_document = True
-hoverxref_auto_ref = True
-hoverxref_roles = [
-    'term',
-]
-
 # Include all your settings here
 html_theme = 'insipid'
 
-js_source_path = '../src/'
 html_extra_path = ['static']
-html_context = {
-    "comment": "This comment is injected manually as a test.",
-}
 
-jinja2_env_kwargs = {
-    "extensions": ["jinja2.ext.loopcontrols"],
-}
+templates_path = ["_templates"]
