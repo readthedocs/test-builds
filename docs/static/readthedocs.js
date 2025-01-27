63b3d301afabaa1a27a9c738539aa4e514ad61df
Manuel Kaufmann <humitos@gmail.com>
2025-01-27 15:13:12 +0100

    Update the custom script

--- docs/static/readthedocs.js
+++ docs/static/readthedocs.js
@@ -1,22 +1,12 @@
-function _handleReadTheDocsData (eventData) {
-  const config = eventData.data();
-
-  if (config.versions.current.hidden) {
-    console.log("This version is HIDDED");
-  } else {
-    console.log("This version IS NOT HIDDEN");
-  }
-}
-
+// Show a message in the console immediatelly loading this script
 console.log("This message comes from a file injected by Cloudflare Worker");
 
-if (window.ReadTheDocsEventData) {
-  // Access Read the Docs Addons data if it's already available when this script is loaded.
-  _handleReadTheDocsData(window.ReadTheDocsEventData);
-} else {
-  // Subscribe to the Read the Docs Addons event to access data if it hasn't been fired yet.
-  document.addEventListener("readthedocs-addons-data-ready", function (event) {
-    _handleReadTheDocsData(event.detail);
-  });
-
+function _handleReadTheDocsData (event) {
+  const config = event.data();
+  console.log("Project slug:", config.projects.current.slug);
 }
+
+// Subscribe to the Read the Docs Addons event to access data if it hasn't been fired yet.
+document.addEventListener("readthedocs-addons-data-ready", function (event) {
+  _handleReadTheDocsData(event.detail);
+});
