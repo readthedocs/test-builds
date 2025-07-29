fa80f098aca9d54f5104c68c0af09b0d13bc589d
Manuel Kaufmann <humitos@gmail.com>
2025-07-29 16:01:31 +0200

    hoverxref is not required anymore

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@ project = 'Test Builds'
 extensions = [
     'sphinx_autorun',
     'notfound.extension',
-    'hoverxref.extension',
     'sphinx_js',
     'sphinx_tabs.tabs',
     'sphinx_jinja2',
@@ -13,10 +12,6 @@ extensions = [
 latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 autosectionlabel_prefix_document = True
-hoverxref_auto_ref = True
-hoverxref_roles = [
-    'term',
-]
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
