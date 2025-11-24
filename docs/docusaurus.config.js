6374bdc1600d63e634e19e1997955dfc1c7906c0
Manuel Kaufmann <humitos@gmail.com>
2025-11-24 11:41:23 +0100

    Use `trailingSlash: true`

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,8 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // Testing trailing slash issue
-  // https://github.com/readthedocs/readthedocs.org/issues/11220#issuecomment-3083219216
+  // Use trailing slash
   // https://docusaurus.io/docs/advanced/routing#routes-become-html-files
   trailingSlash: false,
 
