26e993cfe0283e1f8e2cdd46ac6d15addd6299c2
Manuel Kaufmann <humitos@gmail.com>
2025-11-26 09:54:36 +0100

    Update js for search

--- docs/assets/javascript/readthedocs.js
+++ docs/assets/javascript/readthedocs.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // Trigger Read the Docs' search addon instead of Zensical default
-    document.querySelector(".md-search__button").addEventListener("click", (e) => {
+    document.querySelector(".md-search").addEventListener("click", (e) => {
+        e.preventDefault();
         const event = new CustomEvent("readthedocs-search-show");
         document.dispatchEvent(event);
     });
