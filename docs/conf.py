e86b6fdbca0d179a425177c270f7361cbf95bfcb
Eric Holscher <eric@ericholscher.com>
2026-01-29 15:12:24 +0800

    Include llms.txt in HTML build output

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,6 @@ latex_engine = 'xelatex'  # allow us to build Unicode chars
 
 # Include all your settings here
 html_theme = 'sphinx_rtd_theme'
+
+# Copy llms.txt to the HTML output
+html_extra_path = ['../llms.txt']
